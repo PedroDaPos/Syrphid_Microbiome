32516715b34f3cd399f1ef3ff4b09cd51fab2b9c
Pedro A P Rodrigues <par@email.arizona.edu>
2025-02-03 16:21:12 -0500

    first files upload

---
+++ code/Get_lineage_blast.R
@@ -0,0 +1,36 @@
+# From  siobhonlegan.com/posts/2022-08-03-rstats-blastn/2022-08-03-rstats-blastn.html
+
+library(taxize)
+library(tidyverse)
+setwd("/scratch/pd88715/pacbio_whiteflygc/fastq/Syrphids_only/results/Blast/")
+
+blastn_taxa <- read.table("./ASV_ITS_nt_blast.tsv", sep="\t", header = FALSE)
+output_columns_blast <- c("qseqid", "sseqid", "stitle", "evalue", "bitscore", "pident", "qcovs", "qcovhsp")
+colnames(blastn_taxa) <- output_columns_blast
+
+#Order table and select just the top hit
+blastn_taxa_sort <- data.table::setorder(blastn_taxa, qseqid, -evalue)
+blastn_taxa_top <- blastn_taxa_sort[match(unique(blastn_taxa_sort$qseqid), blastn_taxa_sort$qseqid),]
+
+#Retrieve the genbank ID and lineage information
+
+# get vector of unique accession numbers
+unique_sseqid <- unique(blastn_taxa_top$sseqid)
+unique_sseqid_df <- data.frame(GI_id = unique_sseqid)
+unique_sseqid_df <- unique_sseqid_df %>%
+  mutate(GI_number = str_extract(GI_id, "\\d+")) %>%
+  mutate(GI_number = as.numeric(GI_number))
+
+# search for these in the ncbi database
+ncbi_lineage <- classification(genbank2uid(id = as.character(unique_sseqid_df$GI_number), verbose = TRUE), db = 'ncbi')
+
+# merge output into a data frame
+ncbi_lineage_df <-cbind(ncbi_lineage)
+ncbi_lineage_df$sseqid <- unique_sseqid
+
+# select just which lineage level to keep
+ncbi_lineage_df <- dplyr::select(ncbi_lineage_df, sseqid, query, superkingdom, phylum, class, order, family, genus, species)
+ncbi_lineage_df <- left_join(blastn_taxa_top, ncbi_lineage_df, by = "sseqid")
+
+# save annotated blast data frame
+write.csv(ncbi_lineage_df, "./taxrank_annotated_ITS2_ASV_nt_blast.csv")
---
+++ results/blast/blast_ASV_syrphid.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=blastASV		# Job name (testBowtie2)
+#SBATCH --partition=batch		# Partition name (batch, highmem_p, or gpu_p)
+#SBATCH --ntasks=1			# Run job in single task, by default using 1 CPU core on a single node
+#SBATCH --cpus-per-task=8	 	# CPU core count per task, by default 1 CPU core per task
+#SBATCH --mem=80G			# Memory per node (4GB); by default using M as unit
+#SBATCH --time=5:00:00              	# Time limit hrs:min:sec or days-hours:minutes:seconds
+#SBATCH --output=%x_%j.out		# Standard output log, e.g., testBowtie2_12345.out
+#SBATCH --error=%x_%j.err		# Standard error log, e.g., testBowtie2_12345.err
+#SBATCH --mail-user=pedro.rodrigues@uga.edu    # Where to send mail
+#SBATCH --mail-type=END,FAIL          	# Mail events (BEGIN, END, FAIL, ALL)
+
+
+module load BLAST+/2.14.1-gompi-2023a
+module load ncbiblastdb/20241102
+mkdir blast_results
+blastn -query Syrphid_asv_02_2025.fasta -db nt -evalue 1e-30 -num_threads 8 -max_target_seqs 1 -outfmt '6 qseqid stitle evalue bitscore pident qcovs qcovhsp' -out blast_results/0203205_nt_blast_syrhpid.tsv
